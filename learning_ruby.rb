d350a958de296cd02d1f52458c26f28b293f64be
Kate Rader <katerader5@gmail.com>
2018-03-29 14:12:42 -0400

    Add files via upload

--- learning_ruby.rb
+++ learning_ruby.rb
@@ -61,10 +61,79 @@ puts "Hi #{name}!"
 name = "Kate Rader"
 puts "Hello #{name}, it is nice to see you again."
 
+#to capitalize a text, use:
+print "What is your last name?"
+last_name = gets.chomp
+last_name.capitalize #capitalizes first letter
 
+#Respond to user input using if statements
+if 0<=8
+  print "Ay, 0 is less than 8!"
+end #always use end at the end of an if statement
+#else
+if 0>8
+  print "That's right!"
+else
+  print "That's not right...."
+end
+  #elsif
+if 0 < 8
+  puts "Ay, 0 is less than 8!"
+elsif 0 > 8
+  puts "0 is not greater than 8."
+else
+  puts "Nope."
 
+#Unless statements: (use instead of reversing if/else)
+hungry = false
 
+unless hungry
+puts "I'm writing Ruby programs!"
+else
+  puts "Time to eat!"
+end
+  
+#Assignment and Comparator (just like other languages: = assigns a value, == compares the values)
+true = 2 != 8 #true becausse 2 is not equal to 8
+false = 2 == 8 #false because when compared, 2 is not equal to 8
 
+#all will evaluate to true
+test_1 = 17 > 16
 
+test_2 = 21 < 30
 
+test_3 = 9 >= 9
 
+test_4 = -11 <= 4
+
+#Logical/Boolean Operators: (&&, ||, !)
+#When combining these operators, anything in () is read first
+#(&&: both must be true)
+boolean_3 = 2**3 == 8 && 3**2 == 9
+boolean_3 = true
+
+boolean_1 = 77 < 78 && 77 < 77
+boolean_1 = false
+
+#(||: one or both expressions are true)
+true || true # => true
+true || false # => true
+false || true # => true
+false || false # => false
+
+#(!: makes a true value false, and a false value true)
+boolean_3 = !(700 / 10 == 70)
+boolean_3 = false
+
+bool_1 = !true
+bool_1 = false
+
+#Check to see if input includes something: .include?
+if input.include? "s" #see if input has an 's' in it
+	print "Yes!"
+
+#Global Substitution: .gsub! replaces every instance of something with another thing
+input.gsub!(/s/, "th") #! at the end of .gsub because Ruby will change the string in-place
+
+ 
+#While Loops
